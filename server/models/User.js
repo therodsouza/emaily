93ae12585cef75ea953a5a6fea5502789a87f95d
Rodrigo Evangelista de Souza <vntroso@vntroso.local>
2018-04-20 17:51:20 -0300

    mongoose models

---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+import { model } from "mongoose";
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    googleId: String;
+    
+});
+
+mongoose,model('users', userSchema);
